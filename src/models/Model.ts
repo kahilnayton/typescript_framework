04b724f0187a5e7785ed6744df03cdef83dfe192
kahil nayton <kahilnayton@gmail.com>
2021-04-20 11:41:26 -0400

    refactoring with collection.ts

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -26,18 +26,11 @@ export class Model<T> {
     private attributes: ModelAttributes<T>,
     private events: Events,
     private sync: Sync<T>
-  ) { }
-  get on() {
-    return this.events.on;
-  }
+  ) {}
 
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
